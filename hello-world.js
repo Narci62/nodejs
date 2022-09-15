bb6d91601ac90ef05cdd28b63b88a63debc9dd10
kodonou narcisse <narcimes2@gmail.com>
2022-09-15 15:06:44 +0200

    correction et amelioration

---
+++ hello-world.js
@@ -0,0 +1,44 @@
+let http=require('http')
+let fs=require('fs')
+let url=require('url')
+
+let server = http.createServer()
+
+server.on('request',(request,response)=>{
+    response.writeHead(200)
+    let query=url.parse(request.url,true).query
+    let name=query.name===undefined?'cher ami':query.name
+    fs.readFile('index.html','utf8',(err,data)=>{
+        if(err){
+            response.writeHead(404)
+            response.end('Ce fichier n\'existe')
+        }
+        else{
+             response.writeHead(200,{
+            'content-type':'text/html;charset=utf-8'
+            })
+            data=data.replace('{{name}}',name)
+            response.end(data)
+        }
+
+    })
+    
+    
+    /*
+    fs.readFile('index.html',(err,data)=>{
+        if(err){
+            response.writeHead(404)
+            response.end('Ce fichier n\'existe')
+        }
+        else{
+             response.writeHead(200,{
+            'content-type':'text/html;charset=utf-8'
+            })
+            response.end(data)
+        }
+       
+    }) */
+   
+})
+
+server.listen(80) //80 est le port pour ecouter la reponse du serveur
